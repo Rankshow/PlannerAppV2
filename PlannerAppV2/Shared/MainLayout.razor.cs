723711c1e48e2cff9fb85e4e033a681afa5097f1
Rankshow <uwaomaobinna20@gmail.com>
2024-03-18 11:18:05 +0100

    Setup Mudblazor packages

---
+++ PlannerAppV2/Shared/MainLayout.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace PlannerAppV2.Shared
+{
+    public partial class MainLayout
+    {
+        public bool _drawerOpen = true;
+
+        public void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
