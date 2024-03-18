2f59604852b520e18744ecde6628bcb8c9013faf
Rankshow <uwaomaobinna20@gmail.com>
2024-03-18 13:41:43 +0100

    Added shared class library

---
+++ PlannerAppV2.Share/Response/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PlannerAppV2.Shared.Response
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public bool IsSuccuss { get; set; } 
+    }
+    public class ApiResponse<T> : ApiResponse
+    {
+        public T? Value { get; set; }
+    }
+}
---
+++ PlannerAppV2.Share/Response/ApierrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PlannerAppV2.Shared.Response
+{
+    public class ApierrorResponse
+    {
+        public string Message { get; set;} = string.Empty;
+        public string[]? Errors { get; set; } 
+        public bool IsSuccuss { get; set;}
+    }
+}
